dc8e635af61573035954b298d74abaa162fba919
sabur <saburoloko@gmail.com>
2026-03-23 23:41:36 +0000

    starting over with ruby just practice

---
+++ ruby/basic_data_types_practice.rb
@@ -0,0 +1,31 @@
+# Addition
+1 + 1 
+
+# subtraction
+2 - 1
+
+# multiplication
+2 * 2
+
+# division
+10 / 5 
+13 / 2.0
+
+#  exponent
+2 ** 2
+3 ** 4
+
+# modulus 
+8 % 2
+10 % 4
+
+# to convert an integer to a float
+13.to_f
+
+# to convert a float to an integer
+13.0.to_i
+13.9.to_i
+
+6.even?
+7.even?
+5.odd?
